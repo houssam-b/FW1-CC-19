f2bed64768c1793cb4ac6fdb67347140fb64e736
Houssam Eddine BAHHOUS <houssam-eddine.bahhous@etu.univ-orleans.fr>
2024-11-06 18:52:27 +0100

    Houssam Eddine Bahhous

--- collec_management/models.py
+++ collec_management/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Collec(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
