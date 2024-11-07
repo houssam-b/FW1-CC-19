89cb089b71c65742ff7e6a34c6848761feac1f52
Houssam Eddine BAHHOUS <houssam-eddine.bahhous@etu.univ-orleans.fr>
2024-11-07 22:05:12 +0100

    save

--- collec_management/urls.py
+++ collec_management/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path('about/', views.about, name='about'),
-<<<<<<< HEAD
-=======
     path('all/', views.all, name='all'),
->>>>>>> 47dbf5871ef33e0a7bf0ef6c029d5752bd0a199a
     path('collection/<int:n>', views.collection_detail, name='collection_detail'),
 ]
+
