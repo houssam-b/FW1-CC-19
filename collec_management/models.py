2617c86a09978068986ec656859975cd9adc42f9
Houssam Eddine BAHHOUS <houssam-eddine.bahhous@etu.univ-orleans.fr>
2024-11-07 14:57:45 +0100

    resoudre les conflits

--- collec_management/models.py
+++ collec_management/models.py
@@ -3,11 +3,8 @@ from django.db import models
 class Collec(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-<<<<<<< HEAD
     data = models.DateField(auto_now_add=True)
-=======
     date = models.DateField(null=False)
->>>>>>> dev
 
     def __str__(self):
         return self.title
