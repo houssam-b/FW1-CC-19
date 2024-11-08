9acb967fde3276c3d46e880637e575cc6e7c74c4
Nina Metrouh <nina.metrouh@etu.univ-orleans.fr>
2024-11-08 17:04:32 +0100

    save

--- collec_management/views.py
+++ collec_management/views.py
@@ -23,6 +23,28 @@ def all(request):
     collection = Collec.objects.all()
     return render(request, 'collec_management/all.html',{'collection':collection})
 
+
+#Q7
+
+def new_collection(request):
+    if request.method == 'POST' :
+        form =CollecForm(request.POST)
+        if form.is_valid():
+            collection = form.save(commit=False)
+            collection.date = timezone.now()
+            collection.save()
+            return redirect('all')
+
+
+    else :
+
+            form=CollecForm()
+
+
+
+            return render(request , 'collec_management/new_collection.html' , {'form' : form})
+        
+
 #Q8
 
 def delete_collection(request,n):
