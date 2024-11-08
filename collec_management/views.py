9601818f38eefee0aa71539cc2c373b87396d624
Houssam Eddine BAHHOUS <houssam-eddine.bahhous@etu.univ-orleans.fr>
2024-11-08 16:04:30 +0100

    save

--- collec_management/views.py
+++ collec_management/views.py
@@ -44,28 +44,6 @@ def new_collection(request):
             return render(request , 'collec_management/new_collection.html' , {'form' : form})
 
 
-#Q7
-
-def new_collection(request):
-    if request.method == 'POST' :
-        form =CollecForm(request.POST)
-        if form.is_valid():
-            collection = form.save(commit=False)
-            collection.date = timezone.now()
-            collection.save()
-            return redirect('all')
-
-
-    else :
-
-            form=CollecForm()
-            
-            
-            
-            return render(request , 'collec_management/new_collection.html' , {'form' : form})
-
-
-
 #Q8
 
 def delete_collection(request,n):
